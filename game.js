dbb88ab4d5e6d67c99e6a7bfcafbea1ec20866f8
Becky <bjentwistle@gmail.com>
2023-07-10 18:49:06 +0100

    adding front end files

---
+++ game.js
@@ -0,0 +1,10 @@
+
+window.onload = function(){
+    this.config = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: 0x000000,
+        scene: Scene1
+      }
+      const game = new Phaser.Game(this.config);
+}
