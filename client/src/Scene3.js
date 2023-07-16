e9c5b88c972893d2af0be15a9c5c7881cf58c927
Becky <bjentwistle@gmail.com>
2023-07-16 12:39:49 +0100

    needs some work

--- client/src/Scene3.js
+++ client/src/Scene3.js
@@ -13,23 +13,25 @@ class Scene3 extends Phaser.Scene {
         this.cameras.main.setBackgroundColor(0x0760f0)
         const canvasWidth = this.scale.canvas.width;
        // const canvasHeight = this.scale.canvas.height;
-        const text = this.add.text(canvasWidth/2, 200, "GAME OVER!" ,{ font: '76px'}).setOrigin(0.5)
+        const text = this.add.text(canvasWidth/2, 100, "GAME OVER!" ,{ font: '76px'}).setOrigin(0.5)
 
         //Display name and score
         this.playerName = JSON.parse(sessionStorage.getItem('data')).userName;
         this.finalScore = JSON.parse(sessionStorage.getItem('score')).score;
         
         //display player's final score 
-        this.add.text(canvasWidth/2, 300, this.playerName +" : " + this.finalScore + " pts",{ font: '46px', fill: '#ffffff' }).setOrigin(0.5)
+        this.add.text(canvasWidth/2, 200, this.playerName +" : " + this.finalScore + " pts",{ font: '46px', fill: '#000000' }).setOrigin(0.5)
 
         //LeaderBoard needs to extract names and scores from the database
         const results = []
         const data = fetch('http://localhost:9000/api/scores_db/')
         .then(res => res.json())
         .then(results => {
+            
             //sort results from highest to low
             results.sort((a, b) => b.highScore - a.highScore);
-
+            this.add.text(canvasWidth / 2, 350, "Highest Scores", {font: '32px', fill: '#ffffff'}).setOrigin(0.5);
+            
             let yPosition = 400;
 
             // Loop through the results and display player names and scores
