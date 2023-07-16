2c8ebd7eaaf7c31da603f792e1a4b3d2d86ee9bc
Becky <bjentwistle@gmail.com>
2023-07-16 12:33:37 +0100

    seems to be working

--- client/src/Scene3.js
+++ client/src/Scene3.js
@@ -26,7 +26,26 @@ class Scene3 extends Phaser.Scene {
         const results = []
         const data = fetch('http://localhost:9000/api/scores_db/')
         .then(res => res.json())
-        .then(results => console.log(results))
+        .then(results => {
+            //sort results from highest to low
+            results.sort((a, b) => b.highScore - a.highScore);
+
+            let yPosition = 400;
+
+            // Loop through the results and display player names and scores
+            for (let i = 0; i < 5; i++) {
+                const playerName = results[i].name;
+                const playerScore = results[i].highScore;
+
+                // Display player name and score
+                this.add.text(canvasWidth / 2, yPosition, playerName + ": " + playerScore + " pts", {
+                    font: '32px',
+                    fill: '#ffffff'
+                }).setOrigin(0.5);
+
+                yPosition += 40; // Increment yPosition to show the next player's data below the previous one
+            };
+        })
         .catch(err => console.log(err.response))
 
         
