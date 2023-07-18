f2dc74022ffc2cb763ef8603c7a82881eb9b4479
Becky <bjentwistle@gmail.com>
2023-07-18 15:53:53 +0100

    tidied commented out code

--- client/src/Scene3.js
+++ client/src/Scene3.js
@@ -18,10 +18,6 @@ class Scene3 extends Phaser.Scene {
         this.cameras.main.setBackgroundColor(0x6B1518)
         const canvasWidth = this.scale.canvas.width;
 
-        // Add a keyboard key event to listen for the "y" key press to quit the game
-        // this.input.keyboard.on('keydown-P', this.playAgain, this);
-        // this.add.text(window.innerWidth - 100, 20, "Play again = p", {fontSize: "20px"}).setOrigin(1);
-  
        // const canvasHeight = this.scale.canvas.height;
         const text = this.add.text(canvasWidth/2, 100, "GAME OVER!" ,{ fontFamily: 'Farro', fontSize: "76pt", align: "center", color:"black", position: "absolute", strokeThickness: 5}).setOrigin(0.5)
 
@@ -60,9 +56,6 @@ class Scene3 extends Phaser.Scene {
         .catch(err => console.log(err.response))
         
     }
-    // playAgain(){
-    //     this.scene.start("playGame2");
-    //     }
 
     update(){
 
